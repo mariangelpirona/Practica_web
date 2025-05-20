5475d74e45282e95740d217f6b02e890c0fb3244
mariangelpirona <pironamariangel38@gmail.com>
2025-05-20 12:59:21 -0400

    ok

--- JS/03_Arreglos_Matrices/01_Palabras/script.js
+++ JS/03_Arreglos_Matrices/01_Palabras/script.js
@@ -1,1 +1,5 @@
-function unaFrase ()
+function unaFrase (){   
+let palabras = ["Juan", "corre", "rápidamente", "feliz","parque"];
+let frase = palabras[0] + " "+ palabras[1]+" "+  palabras[2]+ " y "+ palabras[3]+" en el "+ palabras[4]+ ".";
+alert(frase);
+}
---
+++ JS/03_Arreglos_Matrices/02_Frases_con_Numeros/script.js
@@ -0,0 +1,7 @@
+console.log()
+
+function FrasesConNumeros(){
+    let datos = [3, "gatos", "negros", "duermen", "tranquilamente"];
+    let frase = " Los "+ datos[0]+ " "+ datos[1]+" "+ datos[2]+ " "+ datos[3]+" "+datos[4]+".";
+    alert(frase)
+}
