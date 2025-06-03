af9aeaa1c88539e7f120fcecf8ebe2af9424511a
mariangelpirona <pironamariangel38@gmail.com>
2025-06-03 12:57:43 -0400

    ok

--- JS/03_Arreglos_Matrices/05_Construir_Frases/script.js
+++ JS/03_Arreglos_Matrices/05_Construir_Frases/script.js
@@ -12,3 +12,4 @@ function ConstruirFrases(){
     alert(frase)
 
 }
+
---
+++ JS/04 manipulacion_arrays/ejemplo/static/JS/script.js
@@ -0,0 +1,36 @@
+// creacion de Arreglos
+
+let array = ["Xbox", "Nintendo"];
+
+// crear Funciones
+function usarPush(){
+    // Agregar al final
+let entreda = prompt ("Agregar una Consola: " )
+array.push (entreda);
+alert("El nuevo arreglo es: " + array.join(" - "))
+}
+
+function usarPop(){
+// elimina el ultimo  elemento (vamos a alamcenar el dato eliminado)
+let basurero = array.pop();
+
+alert(`Elemento eliminado: ${basurero}`);
+
+}
+
+
+function usarUnshift(){
+ // Agregar al inicio
+let entreda = prompt ("Agregar una Consola: " )
+array.unshift (entreda);
+alert("El nuevo arreglo es: " + array.join(" - "))
+}
+
+
+function usarShift(){
+// elimina el primer  elemento (vamos a alamcenar el dato eliminado)
+let basurero = array.shift();
+
+alert(`Elemento eliminado: ${basurero}`);
+
+}
