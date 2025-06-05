2a94dc0f22933f16db0fc39fc29868b4f7363200
mariangelpirona <pironamariangel38@gmail.com>
2025-06-05 09:28:56 -0400

    ok

---
+++ JS/04 manipulacion_arrays/ejercicio_resuelto/ejercicio_1/static/JS/script.js
@@ -0,0 +1,17 @@
+// Ejercicio 1
+let compras=[];  // arreglo vacio
+
+function ListasCompras(){
+    compras.push("pan","leche","huevos");
+    alert(`Lista de Compras: ${compras.join(", ")}`)
+}
+
+// Ejercicio 2
+
+function EliminarElemento(){
+
+let  basurero = compras.pop();
+alert(`EL Producto eliminado: ${basurero}`)
+
+}
+
