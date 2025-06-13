807c31b86a4151364ff069047e9f8f5d01f4f7be
mariangelpirona <pironamariangel38@gmail.com>
2025-06-13 12:56:13 -0400

    ok

--- JS/05 Condicones IF_ELSE/static/JS/script.js
+++ JS/05 Condicones IF_ELSE/static/JS/script.js
@@ -63,12 +63,93 @@ function validarCola(){
 
 if (cola[0] === "sinNombre") {
     cola.shift()
-document.getElementById("resultado4").innerHTML =`<p> Primer ELemento Eliminado.<br></br> Nueva Cola: <strong>${cola.join(" , ")}</strong></p>`;
+document.getElementById("resultado4").innerHTML =`<p> Primer ELemento Eliminado.<br></br> Nueva Lista: <strong>${cola.join(" , ")}</strong></p>`;
 }
 else if (cola [0] !== "sinNombre"){
     document.getElementById("resultado4").innerHTML =`<p> Cola Correcta:<strong>${cola.join(" , ")}</strong></p>`;
 }
 else{
-    document.getElementById("resultado3").innerHTML =`<p>El valor  <strong>${tareas.length}</strong> es invalido</p>`;
+    document.getElementById("resultado4").innerHTML =`<p>El valor  <strong>${tareas.length}</strong> es invalido</p>`;
 }
 }
+
+//  Ejercicio 5: Saludo condicional
+
+function saludoCondicional(){
+
+nombre= prompt("Ingrese su Nombre: ");
+
+if (nombre.trim () !== "" ){
+    
+let saludos = [];
+
+saludos.push(`Hola, ${nombre}`);
+document.getElementById("resultado5").innerHTML =`<p>${saludos[0]}</p>`;
+
+}
+else{
+    document.getElementById("resultado5").innerHTML =`<p>Nombre no Valido</p>`;
+}
+
+}
+
+
+//  Ejercicio 6: Calificar nota
+
+function calificarNotas(){
+    nota=parseFloat(prompt("Ingrese su nota (un valor entre 7.0 y 0.0)"))
+    if(nota > 7 || nota < 0){
+        document.getElementById("resultado6").innerHTML =`<p><strong>ERROR</strong><br>El valor ingresado como nota no es valido</p>`;
+    }
+    else if(nota >=6){
+        document.getElementById("resultado6").innerHTML =`<p><strong>EXCELENTE</strong></p>`;
+    }
+    else if (nota >=4){
+        document.getElementById("resultado6").innerHTML =`<p><strong>APROBADO</strong></p>`;
+    }
+    else if(nota < 4){
+        document.getElementById("resultado6").innerHTML =`<p><strong>REPROBADO</strong></p>`;
+    }
+}
+
+// 7 Registro de visitantes
+function registroVisitas(){
+    let visitas= [];
+    nombre = prompt("Ingrese su nombre: ")
+
+    if (nombre != ""){
+        visitas.unshift(nombre)
+        document.getElementById("resultado7").innerHTML =`<p>La visita: ${visitas.join(" , ")}</p>`;
+    }
+    else{
+        document.getElementById("resultado7").innerHTML =`<p>ERROR NOMBRE VACIO</p>`;
+    }
+
+}
+
+//  Ejercicio 8: Control de stock
+/* let stock = ["arroz", "fideos"];
+
+Declara la variable productoSolicitado.
+Si stock incluye el producto, muestra "Producto disponible".
+Si no, agrégalo con push() y muestra "Producto agregado al stock".
+ */
+
+function controlStock(){
+    let stock = ["arroz", "fideos"];
+    productoSolicitado= prompt("Ingrese su producto: ")
+
+    if (stock[0] == productoSolicitado){
+        document.getElementById("resultado8").innerHTML =`<p>Producto disponible</p>`;
+    }
+    else if (stock[1] ==productoSolicitado){
+        document.getElementById("resultado8").innerHTML =`<p>Producto disponible</p>`;
+    }
+    else{
+        stock.push(productoSolicitado)
+        document.getElementById("resultado8").innerHTML =`<p>Producto agregado al stock ${stock.join(" , ")}</p>`;
+    }
+}
+
+
+
