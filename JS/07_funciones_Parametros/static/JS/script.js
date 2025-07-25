8bf3e3802f62d84431ebd22fc6c59267c080dea9
mariangelpirona <pironamariangel38@gmail.com>
2025-07-25 12:54:42 -0400

    ok

--- JS/07_funciones_Parametros/static/JS/script.js
+++ JS/07_funciones_Parametros/static/JS/script.js
@@ -22,3 +22,41 @@ function mostrarPalabra(){
 
 
 
+
+/*Ejercicio 2: Comparar dos números
+Usa input para pedir dos números. Crea una función con dos parámetros que compare los valores y retorne un mensaje 
+indicando cuál número es mayor, menor o si son iguales. Muestra el mensaje */ 
+
+
+function compararNumeros (num1, num2){
+
+
+if (numi > num2) {
+return `$(num1) es mayor que $(num2)`;
+}else if (num1 < num2){
+return `${numl} es menor que ${num2}`;
+}
+else{
+return `${numl} es igual que ${num2}`;
+}   
+}                               
+
+function mostrarComparacion(){
+
+let inputl= document.getElementById("numComp1");
+let input2= document.getElementById("numComp2");
+let num1= parseFloat(input1.value);
+let num2 =parseFloat(input2.value);
+
+let resultado= compararNumeros (num1, num2);
+document.getElementById ("resultado2").textContent = resultado;
+
+if (isNaN(num1) || isNaN = (num2)){
+alert("Valor Invalido, Ingrese solo numeros");}
+
+
+inputl.value = ""; // limpia la caja de texto 
+inputl.focus (); // poner foco en elemento 
+input2.value= ""; }
+
+
